13fdcc4580fcaa425ba82087aba085b033845365
douglasmg7 <douglasmg7@gmail.com>
2026-08-07 10:48:11 -0300

    Ownership channel.

---
+++ 05-channel/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	owner := func() <-chan int {
+		ch := make(chan int)
+
+		go func(){
+			defer close(ch)
+			for i := range 5 {
+				ch <- i
+			}
+		}()
+
+		return ch
+	}	
+
+	consumer := func(ch <-chan int) {
+		// read values from channel
+		for v := range ch {
+			fmt.Printf("Received: %d\n", v)
+		}
+		fmt.Println("Done receiving!")
+	}
+
+	ch := owner()
+	consumer(ch)
+}
