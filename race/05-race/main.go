3d4c7ca7a5d09ec91ca78121d874bc35361ecc69
douglasmg7 <douglasmg7@gmail.com>
2026-08-17 20:47:40 -0300

    Race condition detector.

---
+++ race/05-race/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//TODO: identify the data race
+// fix the issue.
+
+func main() {
+	ch := make(chan int)
+	start := time.Now()
+	var t *time.Timer
+	t = time.AfterFunc(randomDuration(), func() {
+		fmt.Println(time.Since(start))
+		ch <- 0
+	})
+
+	for time.Since(start) < 5*time.Second {
+		// fmt.Printf("time: %d -> %d\n", time.Since(start), 5*time.Second)
+		_ = <-ch
+		t.Reset(randomDuration())
+	}
+
+}
+
+func randomDuration() time.Duration {
+	return time.Duration(rand.Int63n(1e9))
+}
+
+//----------------------------------------------------
+// (main goroutine) -> t <- (time.AfterFunc goroutine)
+//----------------------------------------------------
+// (working condition)
+// main goroutine..
+// t = time.AfterFunc()  // returns a timer..
+
+// AfterFunc goroutine
+// t.Reset()        // timer reset
+//----------------------------------------------------
+// (race condition- random duration is very small)
+// AfterFunc goroutine
+// t.Reset() // t = nil
+
+// main goroutine..
+// t = time.AfterFunc()
+//----------------------------------------------------
